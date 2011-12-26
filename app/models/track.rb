df184d1c68ae74c3fbf548c7b9df263c52eab5fd
John Berry <ulfmagnetics@gmail.com>
2011-12-26 13:50:11 -0800

    First round of Paperclip + S3 changes.

--- app/models/track.rb
+++ app/models/track.rb
@@ -8,7 +8,13 @@ class Track < ActiveRecord::Base
   validates_numericality_of :position
   validates_inclusion_of :format, :in => valid_audio_formats
 
-  has_attached_file :media
+  has_attached_file :media,
+    :storage => :s3,
+    :bucket => S3_CONFIG[Rails.env]["bucket"],
+    :s3_credentials => {
+      :access_key_id => S3_CONFIG[Rails.env]["access_key_id"],
+      :secret_access_key => S3_CONFIG[Rails.env]["secret_access_key"]
+    }
   validates_attachment_content_type :media, :content_type => valid_audio_mime_types
   validates_attachment_size :media, :less_than => 250.megabytes
 end
