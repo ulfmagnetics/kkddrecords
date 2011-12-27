2089464311188c15a7b8955cbc6433719341c7b9
John Berry <ulfmagnetics@gmail.com>
2011-12-26 19:06:54 -0800

    Hide the metadata fields on track creation page for now. Will need to do something more interesting here to handle untagged MP3s and WAV files.

--- app/admin/tracks.rb
+++ app/admin/tracks.rb
@@ -20,11 +20,12 @@ ActiveAdmin.register Track do
   end
 
   form :html => {:enctype => 'multipart/form-data'} do |f|
-    f.inputs "Metadata" do
-      f.input :title
-      f.input :position
-      f.input :format, :as => :select, :collection => Track.valid_audio_formats
-    end
+    # TODO conditionally display this if we're uploading a WAV file
+    # f.inputs "Metadata" do
+    #   f.input :title
+    #   f.input :position
+    #   f.input :format, :as => :select, :collection => Track.valid_audio_formats
+    # end
     f.inputs "Media" do
       f.input :media, :as => :file, :hint => "MP3 tags will be used to set title, position, band, album, etc."
     end
